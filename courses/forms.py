8b8df90b7297df01340cbaed4ef1207d581ecce5
stabone <stabone@inbox.lv>
2014-01-25 02:02:26 +0200

    added style for some elements< menu, submit button

--- courses/forms.py
+++ courses/forms.py
@@ -6,6 +6,7 @@ from courses.models import Categorie, Tut
 class CategorieForm(ModelForm):
     class Meta:
         model = Categorie
+        #TODO: attrs={'class':'ui form'} // there should be manualy created form
 
 class TutForm(ModelForm):
     class Meta:
--- courses/models.py
+++ courses/models.py
@@ -24,7 +24,6 @@ class Tut(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     level = models.CharField(max_length=1, choices=LEVELS, default=BEGINNER)
-    # level = models.CharField(max_length=1)
     rating = models.IntegerField()
     tags = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
