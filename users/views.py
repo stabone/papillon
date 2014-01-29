b9212b035c1274d26dfdfc8deb5f49a8b3127527
stabone <stabone@inbox.lv>
2014-01-29 23:59:19 +0200

    added generic login mechanism

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render #, redirect
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_protect
 from django.db import IntegrityError
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrig.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -40,4 +42,34 @@ def register(request):
         form = UserCreationForm()
 
     return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+@csrf_protect
+def edit_profile(request, user_id):
+    # load user data
+    # all goodies in form
+    return render(request, 'user/profile.html', {'form': form})
+
+
+@csrf_protect
+def login_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # redirect to a success page
+        else:
+            # return a 'disabled login' error message
+    else:
+        # return an 'invalid login' error message
+
+
+@login_required
+@csrf_protect
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/course/')
 
