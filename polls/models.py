67551edbcfa502839932fc22bd6ad61e82f0c699
Ivars Naglis <stabone@inbox.lv>
2014-02-06 11:49:00 +0200

    added question table for poll model

--- polls/models.py
+++ polls/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
 class Polls(models.Model):
-    question = models.CharField(max_length=255)
+    poll = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
+        return self.poll
+
+
+class Questions(models.Model):
+    poll_id = models.ForeignKey(Poll, db_index=True)
+    question = models.CharField(max_lenght=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
         return self.question
 
+
 class Choises(models.Model):
-    poll_id = models.ForeignKey(Polls, db_index=True)
+    poll_id = models.ForeignKey(Questions, db_index=True)
     option  = models.CharField(max_length=255)
     choise_type = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
