98ebd4ad7f07be0ad7f5276e2f4f7d8cb1e2b2f4
Ivars Naglis <stabone@inbox.lv>
2014-02-16 06:06:39 -0500

    added 'name' parameter for urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'users.views.index'),
-    url(r'^create/$', 'users.views.create'),
-    url(r'^register/$', 'users.views.register'),
-    url(r'^email/send/$', 'users.views.send_notification'),
+    url(r'^create/$', 'users.views.create', name='create_user'),
+    url(r'^register/$', 'users.views.register', name='register_user'),
+    url(r'^email/send/$', 'users.views.send_notification', name='send_notification'),
 )
