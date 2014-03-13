19ae110713d1052599a953400b44b0c85a3e8f11
Ivars Naglis <stabone@inbox.lv>
2014-03-13 05:46:16 -0400

    users tests case created

--- users/models.py
+++ users/models.py
@@ -8,8 +8,9 @@ def handle_file_upload(instance, filename):
 class Users(models.Model):
     username = models.CharField(max_length=120)
     lastname = models.CharField(max_length=120)
-    email = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=handle_file_upload,max_length=255)
-    location = models.SlugField(max_length=100)
+    email = models.CharField(max_length=255,unique=True)
+    password = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=handle_file_upload,max_length=255,blank=True,null=True)
+    location = models.SlugField(max_length=100,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from users.models import Users
 
-# Create your tests here.
+
+class UsersTest(TestCase):
+    def setUp(self):
+        obj = Users.objects.create(username='ivars',lastname='naglis',email='stabone@inbox.lv')
+        obj.save()
+
+
+    def test_user_creating(self):
+        obj = Users.objects.get(email='stabone@inbox.lv')
+        self.assertEqual(obj.username, 'ivars')
