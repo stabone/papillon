d8855ffc98feb99f7019f4f5cf89add96d190352
Ivars Naglis <stabone@inbox.lv>
2014-07-21 01:00:18 +0300

    added response message sending

--- messaging/views.py
+++ messaging/views.py
@@ -9,7 +9,7 @@ from messaging.forms import ContactForm, MessagingForm
 
 @login_required
 def inbox(request):
-    messages = Messages.objects.filter(trash=False) # by user
+    messages = Messages.objects.filter(trash=False).order_by('-sent_at') # by user
     return render(request, 'message/inbox.html', {'messages': messages})
 
 
@@ -22,7 +22,7 @@ def add_message(request):
             rec = message.save(commit=False)
             rec.user_from = request.user
             rec.save()
-            return redirect('/message/')
+            return redirect(reverse('messaging_inbox'))
     else:
         message = MessagingForm()
 
@@ -34,6 +34,16 @@ def read_message(request,msg_id):
     message = Messages.objects.get(id=msg_id)
 
     return render(request, 'message/read.html', {'message': message})
+
+
+@login_required
+def respond_message(request,msg_id):
+    if request.method == 'GET':
+        responce = get_object_or_404(Messages,id=msg_id)
+        message = Messages(user_to=responce.user_from,title=responce.title)
+        form = MessagingForm(instance=message)
+
+    return render(request, 'message/respond.html', {'form': form})
 
 
 @login_required
