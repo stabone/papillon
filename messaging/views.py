0d51523aa3ff3ef0726304ea5d39a0b058e185db
Ivars Naglis <stabone@inbox.lv>
2014-07-20 21:57:18 +0300

    update for message sending functionality

--- messaging/views.py
+++ messaging/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
+from django.core.urlresolvers import reverse
 
 from messaging.models import Contacts, Messages
 from messaging.forms import ContactForm, MessagingForm
@@ -36,12 +37,30 @@ def read_message(request,msg_id):
 
 
 @login_required
+@csrf_protect
+def restore_message(request):
+    if request.method == 'POST':
+        message_id = request.POST.get('messageID')
+
+        message = Messages.objects.get(id=message_id)
+        message.trash = False
+        message.save()
+
+        return redirect(reverse('messaging_inbox'))
+
+    ## back to tash
+    return redirect(reverse('messaging_trash'))
+
+
+@login_required
 def trash_message(request):
     messages = Messages.objects.filter(trash=True)
 
     return render(request, 'message/trash.html', {'messages': messages})
 
 
+@login_required
+@csrf_protect
 def to_trash_message(request):
     if request.method == "POST":
         messageID = request.POST.get('messageID')
@@ -51,14 +70,16 @@ def to_trash_message(request):
         message.trash = True
         message.save()
 
-        return redirect('/message/')
+        return redirect(reverse('messaging_inbox'))
+
 
 @login_required
+@csrf_protect
 def delete_message(request):
     if request.method == "POST":
         Messages.object.delete()
 
-        return redirect('/message/inbox/')
+        return redirect(reverse('messaging_inbox'))
 
 
 @login_required
