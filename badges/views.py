4cf8bd64ed31ccba8db33ba04757462f17a96f8b
Ivars Naglis <stabone@inbox.lv>
2014-09-01 06:49:12 +0000

    updated for badge editing

--- badges/views.py
+++ badges/views.py
@@ -9,7 +9,7 @@ from badges.models import Badges, BadgeTypes
 
 
 def get_badges(request):
-    badge = Badges.objects.fiter(user=request.user)
+    badges = Badges.objects.fiter(user=request.user)
     return render(request, 'badges/badges.html', {'badges': badges})
 
 
@@ -20,15 +20,39 @@ def add_badge(request):
         badge = BadgeForm(request.POST)
         badge.save()
 
-    return redirect()
+    return redirect(reverse('badge_base'))
 
 
 @login_required
 @csrf_protect
-def edit_badge(request, badge_id):
-    badge = get_object_or_404(Badges, id=badge_id)
+def edit_badge(request, rec_id):
+    try:
+        badge_id = int(rec_id)
+    except ValueError:
+        # izmests 404
+        pass
 
-    return render(request, '', {})
+    badge = get_object_or_404(Badges, id=badge_id)
+    form = BadgeForm(instance=badge)
+
+    return render(request, '', {'form': form})
+
+
+@login_required
+@csrf_protect
+def update_badge(request):
+    if request.method == 'POST':
+
+        badge = get_object_or_404(Badges, id=request.POST.get('badge_id'))
+
+        form = BadgeForm(request.POST, instance=badge)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect(reverse('badge_base'))
+
+    return redirect(reverse('badge_base'))
 
 
 @login_required
